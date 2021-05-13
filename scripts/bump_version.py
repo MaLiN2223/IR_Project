8cbcd85531c07534c97311e3a859f0e7dfbad539
Malin <szyszka45@gmail.com>
2021-05-14 00:27:56 +0200

    x

--- scripts/bump_version.py
+++ scripts/bump_version.py
@@ -1,7 +1,14 @@
 #!/usr/bin/env
+from datetime import datetime
+
 import git
 
 COMMITID = git.Repo().head.object.hexsha[:7]
+now = datetime.now()
+version = COMMITID + " " + now.strftime("%d/%m/%Y %H:%M:%S")
 
-with open("VERSION.txt", "w") as f:
-    f.write(COMMITID)
+with open("api_VERSION.txt", "w") as f:
+    f.write(version)
+
+with open("frontend_VERSION.txt", "w") as f:
+    f.write(version)
