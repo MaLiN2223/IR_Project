192c677c11e061c8c3cbf1d51d9d79b03e754a20
Malin <szyszka45@gmail.com>
2021-05-14 15:27:34 +0200

    x

--- website/src/consts.tsx
+++ website/src/consts.tsx
@@ -1,1 +1,1 @@
-export const baseUrl = process.env.NODE_ENV === 'production' ? 'https://20.73.169.71:8443/' : 'http://127.0.0.1:5000/'
+export const baseUrl = process.env.NODE_ENV === 'production' ? 'http://20.73.169.71:5000/' : 'http://127.0.0.1:5000/'
