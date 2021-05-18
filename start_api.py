68457846e689f0ce048c61f797dc641a26d98b60
Malin <szyszka45@gmail.com>
2021-05-18 21:56:11 +0200

    x

--- start_api.py
+++ start_api.py
@@ -8,8 +8,9 @@ print("Environment", os.environ["FLASK_ENV"], base_path())
 if __name__ == "__main__":
 
     if is_prod():
-        domain = "www.malin.dev"
-        ssl_context = (f"/etc/letsencrypt/live/{domain}/fullchain.pem", f"/etc/letsencrypt/live/{domain}/privkey.pem")
-        app.run(debug=True, port=5000, threaded=True)  # , ssl_context=ssl_context)
+        import logging
+
+        logging.basicConfig(filename="out.log", level=logging.DEBUG)
+        app.run(debug=True, host="0.0.0.0", port=5000, threaded=True)  # , ssl_context=ssl_context)
     else:
         app.run(debug=True)
