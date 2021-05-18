1ceaa443b5ee78923c50b8061ffdd628d9dc4768
Malin <szyszka45@gmail.com>
2021-05-18 19:04:16 +0200

    x

--- start_api.py
+++ start_api.py
@@ -5,16 +5,15 @@ from OpenSSL import SSL
 from backend.api import app
 
 is_prod = os.environ["FLASK_ENV"] == "production"
-print("Environment", os.environ["FLASK_ENV"])
 
 if __name__ == "__main__":
+    print(
+        "Environment",
+    )
     if is_prod:
         domain = "www.malin.dev"
-        context = SSL.Context(SSL.TLSv1_2_METHOD)
-        context.use_privatekey_file(f"/etc/letsencrypt/live/{domain}/privkey.pem")
-        context.use_certificate_chain_file(f"/etc/letsencrypt/live/{domain}/fullchain.pem")
-        context.use_certificate_file(f"/etc/letsencrypt/live/{domain}/cert.pem")
-        app.run(debug=True, port=5000, threaded=True, ssl_context=context)
+        ssl_context = (f"/etc/letsencrypt/live/{domain}/fullchain.pem", f"/etc/letsencrypt/live/{domain}/privkey.pem")
+        app.run(debug=True, port=5000, threaded=True, ssl_context=ssl_context)
 
     else:
         app.run(debug=True)
