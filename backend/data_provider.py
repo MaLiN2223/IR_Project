a56afc80d39d9e3ea61fb8c771630cd78aef5f4d
Malin <szyszka45@gmail.com>
2021-05-18 19:41:28 +0200

    move

--- backend/data_provider.py
+++ backend/data_provider.py
@@ -1,12 +1,12 @@
 import pickle
-from typing import List, Optional
+from typing import Iterable, List, Optional
 
 from gensim.models.fasttext import FastText
 from keybert.model import KeyBERT
 from rank_bm25 import BM25L
 
 from backend.utils import is_prod
-from src.index.utils import load_indexes
+from src.index.contants import dump_ids_file_name, index_file_name
 
 wiki_ft_model = None
 keyword_model: Optional[KeyBERT] = None
@@ -31,9 +31,7 @@ def load_bm25():
 def get_wiki_ft_model():
     global wiki_ft_model
     if wiki_ft_model is None:
-        print("Loading wiki_ft_model")
-        wiki_ft_model = FastText.load("./data/ft/fasttext_300.model")
-        print("Loaded.")
+        wiki_ft_model = FastText.load(f"{base_path()}fasttext_300.model")
     return wiki_ft_model
 
 
@@ -49,3 +47,18 @@ def get_doc_local_id_to_remote_id():
     if doc_local_id_to_remote_id is None:
         doc_local_id_to_remote_id = list(load_indexes())
     return doc_local_id_to_remote_id
+
+
+def load_index(model_name: str):
+    import nmslib
+
+    index = nmslib.init(method="hnsw", space="cosinesimil")
+    index.loadIndex(filename=f"{base_path()}{model_name}/{index_file_name}")
+    return index
+
+
+def load_indexes(model_name: Optional[str] = None) -> Iterable[str]:
+    file_name = f"{base_path()}{model_name}/{dump_ids_file_name}" if model_name is not None else f"{base_path()}{dump_ids_file_name}"
+    with open(file_name, "r", encoding="utf-8") as ids:
+        for line in ids:
+            yield line.strip()
--- start_api.py
+++ start_api.py
@@ -4,12 +4,12 @@ from backend.api import app
 from backend.data_provider import base_path
 from backend.utils import is_prod
 
+print("Environment", os.environ["FLASK_ENV"], base_path())
 if __name__ == "__main__":
-    print("Environment", os.environ["FLASK_ENV"], base_path())
+
     if is_prod():
         domain = "www.malin.dev"
         ssl_context = (f"/etc/letsencrypt/live/{domain}/fullchain.pem", f"/etc/letsencrypt/live/{domain}/privkey.pem")
         app.run(debug=True, port=5000, threaded=True, ssl_context=ssl_context)
-
     else:
         app.run(debug=True)
